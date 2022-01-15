a6879c0c156d345aa74e94d6a1e1401fad1eccc1
Islombek <sultonovislom9717@gmail.com>
2022-01-15 18:09:45 +0500

    first commit

---
+++ precache-manifest.ebf27109cff834bd6aed4b8d9b15f391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fd8c585c9e525580fe602198edc152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b78b95b11caf954e7e2",
+    "url": "/static/css/main.3e30b071.chunk.css"
+  },
+  {
+    "revision": "1f408a1a23c57b59501f",
+    "url": "/static/js/2.24128fb1.chunk.js"
+  },
+  {
+    "revision": "e5cc1775e1472ea89d21",
+    "url": "/static/js/3.f7810a60.chunk.js"
+  },
+  {
+    "revision": "9b78b95b11caf954e7e2",
+    "url": "/static/js/main.4a271096.chunk.js"
+  },
+  {
+    "revision": "a389351047890f625fc3",
+    "url": "/static/js/runtime~main.c5acc6bb.js"
+  }
+]);
